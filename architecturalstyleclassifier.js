5ca8bbe58b59b35ddb853549864ddf2bdd56c70c
areddy831 <79551348+areddy831@users.noreply.github.com>
2021-02-23 23:39:38 -0500

    Add files via upload

--- architecturalstyleclassifier.js
+++ architecturalstyleclassifier.js
@@ -1,5 +1,20 @@
-// load model in js script
-(async () => {
-  ...
-  const model = await tf.loadFrozenModel('saved_model.pb', '/web_model/model.json')
-})()
+model = await tf.loadModel('web_model/model.json')
+
+function preprocess(img)
+{
+
+    //convert the image data to a tensor
+    let tensor = tf.fromPixels(img)
+    //resize to 300 X 300
+    const resized = tf.image.resizeBilinear(tensor, [300, 300]).toFloat()
+    // Normalize the image
+    const offset = tf.scalar(255.0);
+    const normalized = tf.scalar(1.0).sub(resized.div(offset));
+    //We add a dimension to get a batch shape
+    const batched = normalized.expandDims(0)
+    return batched
+
+}
+
+
+const pred = model.predict(preprocess(img)).dataSync()
