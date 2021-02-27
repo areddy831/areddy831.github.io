bcb1d32a91dca73df31b07d273a5c1705b74dd20
areddy831 <79551348+areddy831@users.noreply.github.com>
2021-02-27 11:02:20 -0500

    Add files via upload

--- architecturalstyleclassifier.js
+++ architecturalstyleclassifier.js
@@ -4,10 +4,9 @@ async function run(){
        tf.image.resizeBilinear(image, [300,300]).toFloat();
   const offset = tf.scalar(255.0);
   const normalized = tf.scalar(1.0).sub(resized_image.div(offset));
-  const MODEL_URL = 'web_model/model.json';
+  const MODEL_URL = 'areddy831.github.io/web_model/model.json';
   const model = await tf.loadLayersModel(MODEL_URL);
   const result = model.predict(batchedImage);
   result.print();
-
 }
 run();
