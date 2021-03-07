62f03d47ce59a810668bd9a7c49238339c9cc9dc
areddy831 <79551348+areddy831@users.noreply.github.com>
2021-03-07 13:24:08 -0500

    Delete get_model.py

--- get_model.py
+++
@@ -1,5 +0,0 @@
-from keras.applications.vgg16 import VGG16
-
-model = VGG16(weights='imagenet')
-model.save('models/model_vgg16_imagenet.h5')
-print('Pre-trained VGG16 model with ImageNet weights saved!')
